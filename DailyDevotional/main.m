7fa8b13cc05b21e499ee8bbecf3ed6d6cba8578a
webwerks <webwerks@webwerkss-Mac-mini.local>
2015-05-06 05:42:29 -0700

    Initial Commit

---
+++ DailyDevotional/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DailyDevotional
+//
+//  Created by webwerks on 5/6/15.
+//  Copyright (c) 2015 webwerks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
